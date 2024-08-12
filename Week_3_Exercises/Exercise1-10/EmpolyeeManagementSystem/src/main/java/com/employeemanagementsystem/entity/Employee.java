e9e61184b75587b264ebd5051206ebd452b35d34
Harsh Jaiswal <harsh.jaiswal1302@gmail.com>
2024-08-12 14:11:49 +0530

    Spring Data JPA

--- Week_3_Exercises/Exercise1-10/EmpolyeeManagementSystem/src/main/java/com/employeemanagementsystem/entity/Employee.java
+++ Week_3_Exercises/Exercise1-10/EmpolyeeManagementSystem/src/main/java/com/employeemanagementsystem/entity/Employee.java
@@ -1,15 +1,19 @@
 package com.employeemanagementsystem.entity;
 
-
 import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedBy;
+import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
+@Data
 @Entity
+@Table(name = "employees")
+@EntityListeners(AuditingEntityListener.class)
 public class Employee {
 
     @Id
@@ -17,89 +21,24 @@ public class Employee {
     private Long id;
 
     private String name;
+
     private String email;
-    @CreatedBy
-    private String createdBy;
-    @CreatedDate
-    private LocalDateTime createdDate;
-    @LastModifiedBy
-    private String lastModifiedBy;
-    @LastModifiedDate
-    private LocalDateTime lastModifiedDate;
+
     @ManyToOne
+    @JoinColumn(name = "department_id")
     private Department department;
 
-    // Constructor, getters, and setters
+    @CreatedDate
+    @Column(updatable = false)
+    private Date createdDate;
 
-    public Employee() {}
+    @LastModifiedDate
+    private Date lastModifiedDate;
 
-    public Employee(String name, String email, Department department) {
-        this.name = name;
-        this.email = email;
-        this.department = department;
-    }
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public String getLastModifiedBy() {
-        return lastModifiedBy;
-    }
-
-    public void setLastModifiedBy(String lastModifiedBy) {
-        this.lastModifiedBy = lastModifiedBy;
-    }
-
-    public LocalDateTime getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
+    @LastModifiedBy
+    private String lastModifiedBy;
 }
