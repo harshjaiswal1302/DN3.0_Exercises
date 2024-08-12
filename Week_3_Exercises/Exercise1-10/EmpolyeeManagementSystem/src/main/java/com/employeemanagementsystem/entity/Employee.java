9a562f0eaf1ee30ae861837f998c328cdd990037
Harsh Jaiswal <harsh.jaiswal1302@gmail.com>
2024-08-12 13:27:34 +0530

    Spring Data JPA

--- Week_3_Exercises/Exercise1-10/EmpolyeeManagementSystem/src/main/java/com/employeemanagementsystem/entity/Employee.java
+++ Week_3_Exercises/Exercise1-10/EmpolyeeManagementSystem/src/main/java/com/employeemanagementsystem/entity/Employee.java
@@ -2,6 +2,12 @@ package com.employeemanagementsystem.entity;
 
 
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Employee {
@@ -12,7 +18,14 @@ public class Employee {
 
     private String name;
     private String email;
-
+    @CreatedBy
+    private String createdBy;
+    @CreatedDate
+    private LocalDateTime createdDate;
+    @LastModifiedBy
+    private String lastModifiedBy;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
     @ManyToOne
     private Department department;
 
@@ -57,4 +70,36 @@ public class Employee {
     public void setDepartment(Department department) {
         this.department = department;
     }
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
 }
