90bcd2347dc562af17f13a94d6a8bca778ee07e8
Harsh Jaiswal <harsh.jaiswal1302@gmail.com>
2024-08-12 14:11:15 +0530

    Spring Data JPA

--- Week_3_Exercises/Exercise1-10/EmpolyeeManagementSystem/src/main/java/com/employeemanagementsystem/entity/Department.java
+++ Week_3_Exercises/Exercise1-10/EmpolyeeManagementSystem/src/main/java/com/employeemanagementsystem/entity/Department.java
@@ -1,15 +1,21 @@
 package com.employeemanagementsystem.entity;
 
 import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedBy;
+import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+
+import java.util.Date;
 import java.util.List;
 
+@Data
 @Entity
+@Table(name = "departments")
+@EntityListeners(AuditingEntityListener.class)
 public class Department {
 
     @Id
@@ -17,87 +23,21 @@ public class Department {
     private Long id;
 
     private String name;
-    @CreatedBy
-    private String createdBy;
-    @CreatedDate
-    private LocalDateTime createdDate;
-    @LastModifiedBy
-    private String lastModifiedBy;
-    @LastModifiedDate
-    private LocalDateTime lastModifiedDate;
 
     @OneToMany(mappedBy = "department")
     private List<Employee> employees;
 
-    // Constructor, getters, and setters
+    @CreatedDate
+    @Column(updatable = false)
+    private Date createdDate;
 
-    public Department() {}
+    @LastModifiedDate
+    private Date lastModifiedDate;
 
-    public Department(String name, List<Employee> employees) {
-        this.name = name;
-        this.employees = employees;
-    }
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
 
-    public Long getId() {
-
-        return id;
-    }
-
-    public void setId(Long id) {
-
-        this.id = id;
-    }
-
-    public String getName() {
-
-        return name;
-    }
-
-    public void setName(String name) {
-
-        this.name = name;
-    }
-
-    public List<Employee> getEmployees() {
-        return employees;
-    }
-
-    public void setEmployees(List<Employee> employees) {
-        this.employees = employees;
-    }
-
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public String getLastModifiedBy() {
-        return lastModifiedBy;
-    }
-
-    public void setLastModifiedBy(String lastModifiedBy) {
-        this.lastModifiedBy = lastModifiedBy;
-    }
-
-    public LocalDateTime getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-
+    @LastModifiedBy
+    private String lastModifiedBy;
 }
